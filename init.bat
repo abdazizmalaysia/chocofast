b3dde134802bb76a8d390cdfe3b7a101db60e16f
Jazari Ahmad <jazarie2@gmail.com>
2017-05-19 10:37:44 +0800

    added wget for onedrive and install autoit

--- init.bat
+++ init.bat
@@ -3,3 +3,9 @@ choco install 7zip --yes
 choco install googlechrome --yes
 choco install notepadplusplus --yes
 choco install teamviewer --yes
+choco install wget --yes
+choco install  --yes
+choco install filezilla --yes
+choco install nodejs --yes
+choco install python2 --yes
+wget https://oneclient.sfx.ms/Win/Direct/17.3.6799.0327/OneDriveSetup.exe c:\users\administrator\desktop\

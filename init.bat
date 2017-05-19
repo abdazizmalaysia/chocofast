3d210a378dd3de106f290ab21ce844ea3e746397
Jazari Ahmad <jazarie2@gmail.com>
2017-05-19 10:28:50 +0800

    install goooglechrome, 7zip notepadplusplus team viewer

--- init.bat
+++ init.bat
@@ -1,1 +1,5 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install 7zip --yes
+choco install googlechrome --yes
+choco install notepadplusplus --yes
+choco install teamviewer --yes

f7b0ed2c14b978fd3fd4deac11ea25335f89b817
abdazizmalaysia <33574398+abdazizmalaysia@users.noreply.github.com>
2017-11-20 14:27:10 +0800

    Update init.bat

--- init.bat
+++ init.bat
@@ -8,4 +8,5 @@ choco install  --yes
 choco install filezilla --yes
 choco install nodejs --yes
 choco install python2 --yes
+choco install foxitreader
 wget https://oneclient.sfx.ms/Win/Direct/17.3.6799.0327/OneDriveSetup.exe c:\users\administrator\desktop\
